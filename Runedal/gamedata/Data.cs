60e9a723a871dfcd75eea12aecbba28e7fd8db90
Marcin Adamaszek <marcinjanadamaszek@gmail.com>
2022-08-10 10:02:32 +0200

    add characters json files and their deserialization

--- Runedal/gamedata/Data.cs
+++ Runedal/gamedata/Data.cs
@@ -14,45 +14,82 @@ namespace Runedal.GameData
     {
         public Data() 
         {
-            //make json deserializer ignore letter cames in property names
+            //make json deserializer ignore letter cases in property names
             Options = new JsonSerializerOptions()
             {
                 PropertyNameCaseInsensitive = true
             };
             
             Locations = new List<Location>();
+            Characters = new List<Character>();
            
         }
         public string? FileName { get; set; }
         public string? JsonString { get; set; }
         public JsonSerializerOptions Options { get; set; }
         public List<Location>? Locations { get; set; }
+        public List<Character>? Characters { get; set; }
         public Player? Player { get; set; }
       
         
+        //method reading json file into json string
+        private string JsonToString(string filePath)
+        {
+            string jsonString = string.Empty;
+            FileName = filePath;
+            jsonString = File.ReadAllText(FileName);
+            return jsonString;
+        }
 
-
-        //method loading player from json file
+        //method loading characters from json file
         public void LoadCharacters()
         {
-            FileName = @"C:\Users\adamach\source\repos\Runedal\Runedal\GameData\Json\Player.json";
-            JsonString = File.ReadAllText(FileName);
-            Player = JsonSerializer.Deserialize<Player>(JsonString, Options)!;
+
+            //load player from json file
+            JsonString = JsonToString(@"C:\Users\adamach\source\repos\Runedal\Runedal\GameData\Json\Player.json");
+            Player[] playerArray = JsonSerializer.Deserialize<Player[]>(JsonString, Options)!;
 
             //put player in his starting location
-            Locations!.Find(loc => loc.Name == Player.Start)!.Characters!.Add(Player);
+            PopulateLocations(playerArray);
+            Player = playerArray[0];
+            Characters!.Add(Player);
+
+            //load traders from json
+            JsonString = JsonToString(@"C:\Users\adamach\source\repos\Runedal\Runedal\GameData\Json\Traders.json");
+            Character[] tradersArray = JsonSerializer.Deserialize<Trader[]>(JsonString, Options)!;
+            PopulateLocations(tradersArray);
+
+            //load monsters
+            JsonString = JsonToString(@"C:\Users\adamach\source\repos\Runedal\Runedal\GameData\Json\monsters.json");
+            Character[] monstersArray = JsonSerializer.Deserialize<Monster[]>(JsonString, Options)!;
+            PopulateLocations(monstersArray);
         } 
 
         //method loading locations from json file
         public void LoadLocations()
         {
-            FileName = @"C:\Users\adamach\source\repos\Runedal\Runedal\GameData\Json\locations.json";
-            JsonString = File.ReadAllText(FileName);
+            JsonString = JsonToString(@"C:\Users\adamach\source\repos\Runedal\Runedal\GameData\Json\locations.json");
             Location[] locationsArray = JsonSerializer.Deserialize<Location[]>(JsonString, Options)!;
 
             foreach (var loc in locationsArray)
             {
                 Locations!.Add(loc);
+            }
+        }
+
+        //helper method for pushing loaded characters objects into Characters list and assigning them into their starting location
+        private void PopulateLocations(Character[] charactersArray)
+        {
+            Location startingLocation;
+
+            foreach (var character in charactersArray)
+            {
+                Characters!.Add(character);
+
+                //Assign trader to it's starting location
+                startingLocation = Locations!.Find(loc => loc.Name!.ToLower() == character.Start!.ToLower())!;
+                startingLocation.AddCharacter(character);
+                character.CurrentLocation = startingLocation;
             }
         }
     }

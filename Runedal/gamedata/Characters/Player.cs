dc7b226b245c68b661ecd8d7865b115c8a2652c5
Marcin Adamaszek <marcinjanadamaszek@gmail.com>
2022-08-18 13:09:59 +0200

    add methods for getting effective hp/mp max/regen and add const values for attribute multipliers

--- Runedal/gamedata/Characters/Player.cs
+++ Runedal/gamedata/Characters/Player.cs
@@ -10,6 +10,22 @@ namespace Runedal.GameData.Characters
 {
     public class Player : CombatCharacter
     {
+        //values of multipliers for calculating attributes modifiers
+        private const double MaxHpStrMultiplier = 10;
+        private const double MaxMpIntMultiplier = 8;
+        private const double HpRegenStrMultiplier = 0.1;
+        private const double MpRegenIntMultiplier = 0.3;
+        private const double SpeedAgiMultiplier = 0.1;
+        private const double AttackStrMultiplier = 1.5;
+        private const double AtkSpeedAgiMultiplier = 0.3;
+        private const double AccuracyAgiMultiplier = 0.3;
+        private const double CriticalAgiMultiplier = 0.5;
+        private const double EvasionAgiMultiplier = 0.3;
+        private const double MagicResistanceIntMultiplier = 0.1;
+
+        //values of other multipliers
+        private const double SpeedWeightMultiplier = -0.02;
+
         //default constructor for json deserialization
         public Player() : base()
         {
@@ -64,20 +80,54 @@ namespace Runedal.GameData.Characters
         public Character? InteractsWith { get; set; }
 
         //methods for getting effective statistics (after applying all modifiers)
+        public override double GetEffectiveMaxHp()
+        {
+            double effectiveMaxHp = base.GetEffectiveMaxHp();
+            double strengthModifier = MaxHpStrMultiplier * Strength;
+            effectiveMaxHp += strengthModifier;
+            return effectiveMaxHp;
+        }
+        public override double GetEffectiveMaxMp()
+        {
+            double effectiveMaxMp = base.GetEffectiveMaxMp();
+            double intelligenceModifier = MaxMpIntMultiplier * Intelligence;
+            effectiveMaxMp += intelligenceModifier;
+            return effectiveMaxMp;
+        }
+        public override double GetEffectiveHpRegen()
+        {
+            double effectiveHpRegen = base.GetEffectiveHpRegen();
+            double strengthModifier = HpRegenStrMultiplier * Strength;
+            effectiveHpRegen += strengthModifier;
+            return effectiveHpRegen;
+        }
+        public override double GetEffectiveMpRegen()
+        {
+            double effectiveMpRegen = base.GetEffectiveMpRegen();
+            double intelligenceModifier = MpRegenIntMultiplier * Intelligence;
+            effectiveMpRegen += intelligenceModifier;
+            return effectiveMpRegen;
+        }
         public override double GetEffectiveSpeed()
         {
             //effective stat with external modifiers
             double effectiveSpeed = base.GetEffectiveSpeed();
 
             //attribute modifier
-            double agilityModifier = Agility * 0.1;
+            double agilityModifier = Agility * SpeedAgiMultiplier;
 
             //weight modifier
-            double weightModifier = GetCarryWeight() * -0.02;
+            double weightModifier = GetCarryWeight() * SpeedWeightMultiplier;
             weightModifier += (Strength * 0.05);
 
             //add modifiers to effective stat
             effectiveSpeed += agilityModifier += weightModifier;
+
+            //prevent effectiveSpeed from dropping below 0
+            if (effectiveSpeed < 0)
+            {
+                effectiveSpeed = 0;
+            }
 
             return effectiveSpeed;
         }
@@ -87,7 +137,7 @@ namespace Runedal.GameData.Characters
             double effectiveAttack = base.GetEffectiveAttack();
 
             //attribute modifier
-            double strengthModifier = Strength * 1.5;
+            double strengthModifier = Strength * AttackStrMultiplier;
 
             //weapon modifier
             double weaponModifier = Weapon!.Attack;
@@ -100,21 +150,21 @@ namespace Runedal.GameData.Characters
         public override double GetEffectiveAtkSpeed()
         {
             double effectiveAtkSpeed = base.GetEffectiveAtkSpeed();
-            double agilityModifier = Agility * 0.3;
+            double agilityModifier = Agility * AtkSpeedAgiMultiplier;
 
             return effectiveAtkSpeed;
         }
         public override double GetEffectiveAccuracy()
         {
             double effectiveAccuracy = base.GetEffectiveAccuracy();
-            double agilityModifier = Agility * 0.3;
+            double agilityModifier = Agility * AccuracyAgiMultiplier;
             effectiveAccuracy += agilityModifier;
             return effectiveAccuracy;
         }
         public override double GetEffectiveCritical()
         {
             double effectiveCritical = base.GetEffectiveCritical();
-            double agilityModifier = Agility * 0.5;
+            double agilityModifier = Agility * CriticalAgiMultiplier;
             effectiveCritical += agilityModifier;
             return effectiveCritical;
         }
@@ -128,14 +178,14 @@ namespace Runedal.GameData.Characters
         public override double GetEffectiveEvasion()
         {
             double effectiveEvasion = base.GetEffectiveEvasion();
-            double agilityModifier = Agility * 0.3;
+            double agilityModifier = Agility * EvasionAgiMultiplier;
             effectiveEvasion += agilityModifier;
             return effectiveEvasion;
         }
         public override double GetEffectiveMagicResistance()
         {
             double effectiveMagicResistance = base.GetEffectiveMagicResistance();
-            double intelligenceModifier = Intelligence * 0.1;
+            double intelligenceModifier = Intelligence * MagicResistanceIntMultiplier;
             effectiveMagicResistance += intelligenceModifier;
             return effectiveMagicResistance;
         }

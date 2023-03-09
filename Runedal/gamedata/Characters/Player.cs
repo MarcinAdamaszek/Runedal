d9f79a4f758db7adbe8e0a35957671d6d0576abd
Marcin Adamaszek <marcinjanadamaszek@gmail.com>
2023-03-09 17:37:57 +0100

    decrease atkspeed agi multiplier from 1.3 to 1.2

--- Runedal/gamedata/Characters/Player.cs
+++ Runedal/gamedata/Characters/Player.cs
@@ -22,7 +22,7 @@ namespace Runedal.GameData.Characters
         private const double MpRegenIntMultiplier = 7;
         private const double SpeedAgiMultiplier = 0.5;
         private const double AttackStrMultiplier = 2;
-        private const double AtkSpeedAgiMultiplier = 1.3;
+        private const double AtkSpeedAgiMultiplier = 1.2;
         private const double AccuracyAgiMultiplier = 1.5;
         private const double CriticalAgiMultiplier = 3.6;
         private const double EvasionAgiMultiplier = 0.5;
